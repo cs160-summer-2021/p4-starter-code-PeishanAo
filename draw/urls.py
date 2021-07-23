782fa99de019a0e6b8ef996e252c93963ae3ffb0
PeishanAo <peishan.ao@gmail.com>
2021-07-23 15:20:05 -0700

    start 4.9

--- draw/urls.py
+++ draw/urls.py
@@ -6,5 +6,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<str:room_name>/', views.room, name='room'),
-
+    path('home', views.home, name='home'),
+    path('search1', views.search1, name='search1'),
+    path('search2', views.search2, name='search2'),
+    path('info', views.info, name='info'),
 ]
--- draw/views.py
+++ draw/views.py
@@ -8,3 +8,15 @@ def room(request, room_name):
     return render(request, 'draw/room.html', {
         'room_name': room_name
     })
+
+def home(request):
+    return render(request, 'draw/home.html')
+
+def search1(request):
+    return render(request, 'draw/search1.html')
+
+def search2(request):
+    return render(request, 'draw/search2.html')
+
+def info(request):
+    return render(request, 'draw/info.html')
